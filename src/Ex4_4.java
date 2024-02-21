64801a9b98dbbd186d9b34e532ef8ca401dd7fbf
Axis <erlia@naver.com>
2024-02-22 08:33:42 +0900

    1

---
+++ src/Ex4_4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Ex4_4 {
+
+	public static void main(String[] args) {
+		int score=0;
+		char grade=' ';
+		
+		System.out.println("점수를 입력하세요.>");
+		
+		Scanner scan=new Scanner(System.in);
+		score=scan.nextInt();
+		
+		if(score >= 90) {
+			grade='A';
+		}else if(score >=80) {
+		    grade='B';
+		}else if(score >= 70) {
+			grade= 'C';
+		}else {
+			grade='D';
+		}
+		
+		System.out.println("당신의 등급은 "+grade+"등급입니다.");
+		
+	}
+	
+
+}
