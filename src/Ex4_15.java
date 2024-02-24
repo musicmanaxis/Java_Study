664bb50e3d794e7a5056af8d0f19116fed0dc80a
Axis <erlia@naver.com>
2024-02-24 09:27:12 +0900

    1

---
+++ src/Ex4_15.java
@@ -0,0 +1,16 @@
+//중첩 for문
+//구구단
+public class Ex4_15 {
+
+	public static void main(String[] args) {
+		
+		for(int i=2;i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+"*" + j + "="+(i*j));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
